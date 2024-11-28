9592d23da7cf1ac5e2d1be612969ec4707ac75ba
Joshua Reed <11220408+jreed1701@users.noreply.github.com>
2024-11-28 12:19:59 -0700

    Add ability to plot using existing means.
    Add a test script and use some test files.

---
+++ scripts/README.md
@@ -0,0 +1,9 @@
+# Scripts
+
+The purpose of the scripts folder is to be a location where developers can test out new ideas and
+build functional code blocks prior to integration with the package command line interface.
+
+# Available Scripts
+
+1. plot_example.py - This script simply creates a data loader, adds some test files, and runs the
+   existing plot utility.
--- src/deepnado/common/constants.py
+++ src/deepnado/common/constants.py
@@ -6,6 +6,15 @@ CONFIG_FILE_NAME = ".dnrc"
 # List all potential input variables
 ALL_VARIABLES = ["DBZ", "VEL", "KDP", "RHOHV", "ZDR", "WIDTH"]
 
+VARIABLES_TO_LABELS = {
+    "DBZ": "dBZ",
+    "VEL": "m/s",
+    "KDP": "degrees/km",
+    "RHOHV": "correlation",
+    "ZDR": "dB",
+    "WIDTH": "m/s",
+}
+
 LOG_LEVELS_MAP = {
     "critical": logging.CRITICAL,
     "error": logging.ERROR,
