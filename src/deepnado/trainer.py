f12fe50613177f0b1d4dee90779648b465dcd4f3
Akele Knecht <akeleknecht@gmail.com>
2025-01-23 13:11:24 -0700

    Changed the loss function (again)

--- src/deepnado/trainer.py
+++ src/deepnado/trainer.py
@@ -93,7 +93,7 @@ class LightningWrapper(pl.LightningModule):
         self.lr_decay_rate=config["lr_decay_rate"]
         self.lr_decay_steps=config["lr_decay_steps"]
         if config["loss"] == "cce":
-            self.loss = nn.BCEWithLogitsLoss(label_smoothing=config["label_smooth"]) # CrossEntropyLoss
+            self.loss = nn.BCEWithLogitsLoss() # CrossEntropyLosslabel_smoothing=config["label_smooth"]
         elif config["loss"] == "hinge":
             self.loss = nn.HingeEmbeddingLoss() # probably need to convert labels to -1, 1 if using this?
         elif config["loss"] == "mae":
